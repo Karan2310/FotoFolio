eb2919f9a64f33c39f223f135b11d7c588070ee5
Karan2310 <karandeep9619@gmail.com>
2023-09-01 16:37:08 +0530

    config updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,1 @@
-// export const SERVER_URL = "https://foto-folio-beige.vercel.app";
-export const SERVER_URL = "http://192.168.0.110:8001";
+export const SERVER_URL = "https://foto-folio-beige.vercel.app";
